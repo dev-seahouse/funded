cc4258f4bd495d7f443c4cb0b2263aaeb9206ca9
kenan <xinkenan.11@hotmail.com>
2016-09-05 01:46:54 +0800

    fix wiredep error

---
+++ app/inc/tail.php
@@ -0,0 +1,14 @@
+<!-- build:js scripts/vendor.js -->
+<!-- bower:js -->
+<script src="/bower_components/jquery/dist/jquery.js"></script>
+<script src="/bower_components/tether/dist/js/tether.js"></script>
+<script src="/bower_components/bootstrap/dist/js/bootstrap.js"></script>
+<!-- endbower -->
+<!-- endbuild -->
+
+<!-- build:js scripts/plugins.js -->
+<!-- endbuild -->
+
+<!-- build:js scripts/main.js -->
+<script src="scripts/main.js"></script>
+<!-- endbuild -->
