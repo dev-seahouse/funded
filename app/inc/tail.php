8ab4a3c79d86802eb0b525723c48c4e79bd50e2e
kenan <xinkenan.11@hotmail.com>
2016-09-05 02:34:22 +0800

    wiredep working

---
+++ app/inc/tail.php
@@ -0,0 +1,16 @@
+<!-- build:js scripts/vendor.js -->
+<!-- bower:js -->
+<script src="/bower_components/modernizr/modernizr.js"></script>
+<script src="/bower_components/jquery/dist/jquery.js"></script>
+<script src="/bower_components/tether/dist/js/tether.js"></script>
+<script src="/bower_components/bootstrap/dist/js/bootstrap.js"></script>
+<script src="/bower_components/PACE/pace.js"></script>
+<!-- endbower -->
+<!-- endbuild -->
+
+<!-- build:js scripts/plugins.js -->
+<!-- endbuild -->
+
+<!-- build:js scripts/main.js -->
+<script src="scripts/main.js"></script>
+<!-- endbuild -->
