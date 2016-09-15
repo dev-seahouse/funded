4e8e6fb32e6826d8d19e53f6c1f4685b60e1e0cf
githubxxcc <xuchen727@hotmail.com>
2016-09-15 14:27:55 +0800

    Change config.php from localhost to 127.0.0.1

--- app/_config/config.php
+++ app/_config/config.php
@@ -1,5 +1,5 @@
 <?php
-const _HOST = "mysql:dbname=funded_db;host=localhost";
+const _HOST = "mysql:dbname=funded_db;host=127.0.0.1";
 const _USER = "root";
 const _PASSWD = "";
 const _DB_NAME = "funded_db";
